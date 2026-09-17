a4c7d75834a9149f384d8b35243eb7900f384929
joshmal128 <joshua_malinsky128@flatbush.org>
2026-09-17 20:34:28 +0000

    SO3 Homework

--- src/HelloYou.java
+++ src/HelloYou.java
@@ -10,9 +10,9 @@
  */
 public class HelloYou {
     public static void main(String[] args) {
-        System.out.println("Josh");
-        System.out.println("I am in 11th grade");
-        System.out.println("I love sports and I love coding");
+        System.out.println("--------");
+        System.out.println("| Josh |");
+        System.out.println("--------");
         // Your code here
 
     }
